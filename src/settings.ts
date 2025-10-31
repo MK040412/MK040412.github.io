c68b34617809d40feeec842fc220d5300299709a
MK040412 <mkkang0412@gmail.com>
2025-11-01 02:52:47 +0900

    Fix: Correct site URL and base path for deployment

--- src/settings.ts
+++ src/settings.ts
@@ -23,14 +23,14 @@ export const social = {
 }
 
 export const template = {
-	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+	website_url: 'https://mk040412.github.io', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
 	lightTheme: 'night', // Select one of the Daisy UI Themes or create your own
 	darkTheme: 'night', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
 	postPerPage: 5,
-    base: '/MK040412.github.io' // Repository name starting with /
+    base: '' // Repository name starting with /
 }
 
 export const seo = {
